289ccd2338f2648cf14b89b8fc78ce63bd9a3953
EliVallejos93 <108695762+EliVallejos93@users.noreply.github.com>
2025-05-23 17:36:58 -0300

    Actualiza la URL de la API en producción
    Se ha comentado la URL de la API de producción anterior y se ha añadido una nueva URL de API interna. Este cambio mejora la seguridad y la eficiencia en el acceso a los servicios backend en el entorno de producción.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://juego-impostor-backend-production.up.railway.app',
+  //apiUrl: 'https://backend-production-6dba.up.railway.app',
+  apiUrl: 'juego-impostor-backend.railway.internal',
   version: '1.0.0',
   featureFlags: {
     enableNewFeature: false,
