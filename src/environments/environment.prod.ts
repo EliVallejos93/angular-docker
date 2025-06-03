c67484131d32fffd06923257352cf4ed7dd2d9b4
EliVallejos93 <108695762+EliVallejos93@users.noreply.github.com>
2025-06-03 19:38:36 -0300

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://backend-production-6dba.up.railway.app',
-  //apiUrl: 'juego-impostor-backend.railway.internal',
+  //apiUrl: 'https://backend-production-6dba.up.railway.app',
+  apiUrl: 'juego-impostor-backend.railway.internal',
   version: '1.0.0',
   featureFlags: {
     enableNewFeature: false,
